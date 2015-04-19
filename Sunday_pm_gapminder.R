89aada02847d99d55c726aa20b55d7067feba8b9
tscialo <tj.scialo@gmail.com>
2015-04-19 15:00:55 -0500

    Iris dataset and year summarization exerise

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -3,7 +3,7 @@
 # April 19, 2015
 
 #Install dplyr packages and dependencies
-install.packages("dplyr", dependencies = TRUE)
+#install.packages("dplyr", dependencies = TRUE)
 
 #Load Dplyr library into working environment
 library("dplyr")
@@ -13,3 +13,44 @@ library("dplyr")
 gap.in <- read.table("output/combined_gapMinder.tsv",
                      sep = "\t", 
                      header= TRUE)
+
+gap.in %>%
+  filter(pop > 15000000) %>%
+  select(country, year, pop) %>%
+  group_by(country) %>%
+  summarize(min = min(pop))
+
+gap.in %>%
+  filter(country == "China") %>%
+  select(country, year, pop)
+
+#calculate the mean population per continent per year for all years prior to 1990
+#and displays in console with no display limit
+gap.in %>%
+  filter(year < 1991) %>%
+  group_by(continent,year) %>%
+  summarize(mean(pop)) %>%
+  as.data.frame()
+
+#calculate the mean population per continent per year for all years prior to 1990
+#and exports to a file
+gap.in %>%
+  filter(year < 1991) %>%
+  group_by(continent,year) %>%
+  summarize(mean(pop)) -> data.out
+
+#samples the exported data
+data.out[2:3,]
+
+attach(iris)
+iris
+
+#install tidyr package
+#install.packages("tidyr", dependencies = TRUE)
+
+#load tidyr package
+library("tidyr")
+
+#Turn iris dataset from wide into long format
+iris.long <- gather(iris, "Measurement", "Value", 1:4)
+
